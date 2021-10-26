c8ea3c4959509f023d2ed91f6b346baaf31bed18
jojojohhh <qwhtkdwp@gmail.com>
2021-10-27 08:20:09 +0900

    Initial commit

---
+++ src/main/java/com/spring/jdbc/JdbcApplication.java
@@ -0,0 +1,10 @@
+package com.spring.jdbc;
+
+import org.springframework.boot.SpringApplication;
+
+public class JdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcApplication.class, args);
+    }
+}
