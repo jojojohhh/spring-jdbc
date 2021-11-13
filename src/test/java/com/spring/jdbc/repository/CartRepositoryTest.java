3202403b0cdce23a54014c5cde434f8df0abefdd
jojojohhh <qwhtkdwp@gmail.com>
2021-11-14 01:04:31 +0900

    cart test

---
+++ src/test/java/com/spring/jdbc/repository/CartRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.spring.jdbc.repository;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(JdbcCartRepository.class)
+public class CartRepositoryTest {
+
+    @Autowired
+    private JdbcCartRepository cartRepository;
+}
